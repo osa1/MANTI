4d694e50d28a0b809f506eac5126eef1d5662750
osa1 <omeragacan@gmail.com>
2013-05-15 14:34:30 +0300

    minor refactoring, helper functions

--- src/Unify.hs
+++ src/Unify.hs
@@ -10,7 +10,6 @@ import Control.Monad.Error
 import Control.Monad.Identity
 
 type Substs    = M.Map String Term
-type Error e a = ErrorT e Identity a
 
 nullSubst :: Substs
 nullSubst = M.empty
@@ -23,7 +22,7 @@ varBind substs var term =
 functorName :: String -> Int -> String
 functorName s i = s ++ "/" ++ show i
 
-unify' :: Substs -> Term -> Term -> ErrorT MantiError Identity Substs 
+unify' :: Substs -> Term -> Term -> ErrorT MantiError Identity Substs
 unify' substs a1@(Atom atom1) a2@(Atom atom2)
   | atom1 == atom2 = return substs
   | otherwise      = throwError $ UnificationError a1 a2
@@ -36,13 +35,16 @@ unify' substs (Compound fName1 terms1) (Compound fName2 terms2)
        in throwError $ UnificationError (Var functor1) (Var functor2)
   | otherwise = do
       substs' <- unify' substs (Atom fName1) (Atom fName2)
-      foldM unify'' substs' (zip terms1 terms2)
+      foldM unify_fold substs' (zip terms1 terms2)
       where
-        unify'' :: Substs -> (Term, Term) -> ErrorT MantiError Identity Substs
-        unify'' substs (t1, t2) = unify' substs t1 t2
+        unify_fold :: Substs -> (Term, Term) -> ErrorT MantiError Identity Substs
+        unify_fold ss (t1, t2) = unify' ss t1 t2
 unify' _ VGen{} _ = error "VGen in unify."
 unify' _ _ VGen{} = error "VGen in unify."
 unify' _ t1 t2 = throwError $ UnificationError t1 t2
 
-unifyWSubsts :: MonadError MantiError m => Substs -> Term -> Term -> m Substs
-unifyWSubsts substs t1 t2 = either throwError return $ runIdentity (runErrorT (unify' substs t1 t2))
+unify'' :: MonadError MantiError m => Substs -> Term -> Term -> m Substs
+unify'' substs t1 t2 = either throwError return $ unify substs t1 t2
+
+unify :: Substs -> Term -> Term -> Either MantiError Substs
+unify substs t1 t2 = runIdentity (runErrorT (unify' substs t1 t2))
