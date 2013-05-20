16e75d982ccd9cdc327a4252b167f9548e693a84
osa1 <omeragacan@gmail.com>
2013-05-20 11:54:24 +0300

    various corrections in unification, Apply instance of Query

--- src/Unify.hs
+++ src/Unify.hs
@@ -1,11 +1,12 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving, FlexibleContexts #-}
 module Unify
-  ( mgu
-  , nullSubst
+  ( nullSubst
+  , unify
   , apply
   , Substs
   , unionSubsts
+  , unionSubsts'
   ) where
 
 import           Types
@@ -34,6 +35,9 @@ instance Apply Term where
 instance Apply Compound where
     apply substs (Compound fName args) = Compound fName (apply substs args)
 
+instance Apply Query where
+    apply substs (Query c) = Query (apply substs c)
+
 instance Apply a => Apply [a] where
     apply substs l = map (apply substs) l
 
@@ -51,9 +55,11 @@ varBind v t ss
   | occursCheck v t = throwError OccursCheck
   | otherwise       = Right $ M.insert v t ss
 
+unionSubsts :: Substs -> Substs -> Either MantiError Substs
+unionSubsts s1 s2 = foldM (flip $ uncurry varBind) s1 (M.toList s2)
 
-unionSubsts :: Substs -> Substs -> Substs
-unionSubsts = M.foldWithKey varBind'
+unionSubsts' :: Substs -> Substs -> Manti Substs
+unionSubsts' s1 s2 = either throwError return $ unionSubsts s1 s2
 
 functorName :: Atom -> Int -> String
 functorName (Atom s) i = s ++ "/" ++ show i
@@ -62,8 +68,8 @@ unify' :: Substs -> Term -> Term -> ErrorT MantiError Identity Substs
 unify' substs a1@(TAtom atom1) a2@(TAtom atom2)
   | atom1 == atom2 = return substs
   | otherwise      = throwError $ UnificationError a1 a2
-unify' substs (TVar var) term = return $ varBind substs var term
-unify' substs term (TVar var) = return $ varBind substs var term
+unify' substs (TVar var) term = either throwError return $ varBind var term substs
+unify' substs term (TVar var) = either throwError return $ varBind var term substs
 unify' substs (TComp (Compound fName1 terms1)) (TComp (Compound fName2 terms2))
   | length terms1 /= length terms2 =
       let functor1 = functorName fName1 (length terms1)
@@ -74,13 +80,10 @@ unify' substs (TComp (Compound fName1 terms1)) (TComp (Compound fName2 terms2))
       foldM unify_fold substs' (zip terms1 terms2)
       where
         unify_fold :: Substs -> (Term, Term) -> ErrorT MantiError Identity Substs
-        unify_fold ss (t1, t2) = unify' ss (apply ss t1) (apply ss t2)
+        unify_fold ss (t1, t2) = unify' ss t1 t2
 unify' _ TVGen{} _ = error "VGen in unify."
 unify' _ _ TVGen{} = error "VGen in unify."
 unify' _ t1 t2 = throwError $ UnificationError t1 t2
 
 unify :: Substs -> Term -> Term -> Either MantiError Substs
 unify substs t1 t2 = runIdentity (runErrorT (unify' substs t1 t2))
-
-mgu :: Substs -> Term -> Term -> Either MantiError Substs
-mgu substs t1 t2 = unify substs (apply substs t1) (apply substs t2)
