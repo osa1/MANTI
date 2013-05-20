26e7894a8136376d97eb787414cc2aa37ff75aef
osa1 <omeragacan@gmail.com>
2013-05-20 17:59:09 +0300

    edit loaded rules with :edit cmd

--- src/REPL.hs
+++ src/REPL.hs
@@ -3,47 +3,74 @@ module REPL where
 
 import Types
 import Parser
+import DFS
+import Print
 
 import Text.Parsec (parse, many)
 
 import System.Environment (getArgs)
+import System.Directory (getTemporaryDirectory, removeFile)
+import System.Cmd (system)
+import System.IO
 import Control.Monad.State
 import Control.Monad.Error
-import System.IO
 
-import DFS
 
-runFile :: FilePath -> IO ()
+runFile :: FilePath -> Manti ()
 runFile path = do
-    contents <- readFile path
-    case parse (many queryOrRule) path contents of
-      Left parseError -> print parseError
-      Right stats -> do
-        r <- manti $ forM_ stats $ \stat ->
-          case stat of
-            Right rule' -> runRule rule'
-            Left query' -> do
-              r <- solve [query']
-              liftIO $ print r
-        print r
+    contents <- liftIO $ readFile path
+    loadFileFromString contents
 
-mantiRepl :: Manti ()
-mantiRepl = do
+repl :: Manti ()
+repl = do
     liftIO $ putStr "?- "
     liftIO $ hFlush stdout
     input <- liftIO getLine
-    case parse queryOrRule "repl" input of
-      Left parseError -> liftIO (print parseError) >> mantiRepl
-      Right (Right rule') -> runRule rule' >> mantiRepl
-      Right (Left query') -> do
+    case parse toplevel "repl" input of
+      Left parseError -> liftIO (print parseError) >> repl
+      Right (TRule rule') -> runRule rule' >> repl
+      Right (TQuery query') -> do
         r <- solve [query']
         liftIO $ print r
-        mantiRepl
+        repl
+      Right (TCmd Edit) -> edit >> repl
 
 runRule :: Rule -> Manti ()
 runRule r = do
     let r' = generalize r
     addRule r'
+
+edit :: Manti ()
+edit = do
+    tempDir <- liftIO $ getTemporaryDirectory
+    (path, handle) <- liftIO $ openTempFile tempDir "temp.manti"
+    rls <- gets rules
+    liftIO $ do
+      hPutStr handle (pprint rls)
+      hFlush handle
+    void $ liftIO $ system $ "vim " ++ path
+    loadFileFromHandle handle
+    liftIO $ do
+      hClose handle
+      removeFile path
+
+loadFileFromHandle :: Handle -> Manti ()
+loadFileFromHandle handle = do
+    contents <- liftIO $ hGetContents handle
+    loadFileFromString contents
+
+loadFileFromString :: String -> Manti ()
+loadFileFromString s = do
+    put defaultMantiState
+    case parse (many queryOrRule) "string" s of
+      Left parseError -> liftIO $ print parseError
+      Right stats -> do
+        forM_ stats $ \stat ->
+          case stat of
+            Right rule' -> runRule rule'
+            Left query' -> do
+              r <- solve [query']
+              liftIO $ print r
 
 manti :: Manti a -> IO (Either MantiError a)
 manti m = evalStateT (runVarGenT (runErrorT (evalStateT (runManti m) defaultMantiState))) 0
@@ -51,8 +78,7 @@ manti m = evalStateT (runVarGenT (runErrorT (evalStateT (runManti m) defaultMant
 main :: IO ()
 main = do
     args <- getArgs
-    if not (null args)
-      then runFile (head args)
-      else do
-        r <- manti mantiRepl
-        print r
+    r <- if not (null args)
+           then manti $ runFile (head args)
+           else manti repl
+    print r
