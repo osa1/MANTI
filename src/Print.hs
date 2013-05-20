0aa31695ad72c1c4abbc21c6b8c537cefae7b854
osa1 <omeragacan@gmail.com>
2013-05-20 18:21:47 +0300

    print VGens, don't print true() goals

--- src/Print.hs
+++ src/Print.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE MultiWayIf #-}
 module Print where
 
 import Types
@@ -18,11 +19,13 @@ instance PPrint Var where
     pprint (Var s) = s
 
 instance PPrint Rule where
+    pprint (Rule (RHead (Atom "true") []) (RBody [])) = ""
     pprint (Rule (RHead rname args) (RBody conjs)) =
       concat [ pprint rname, "(", intercalate ", " (map pprint args), ")"
-             , if null conjs
-                 then "."
-                 else concat [ " :- "
+             , if | null conjs -> "."
+                  | conjs == [Compound (Atom "true") []] -> "."
+                  | otherwise ->
+                      concat [ " :- "
                              , intercalate ", " (map pprint conjs)
                              , "."
                              ]
@@ -32,7 +35,7 @@ instance PPrint Term where
     pprint (TAtom a) = pprint a
     pprint (TVar v) = pprint v
     pprint (TComp comp) = pprint comp
-    pprint TVGen{} = error "TVGen in pprint"
+    pprint (TVGen i) = "_V" ++ show i
 
 instance PPrint Compound where
     pprint (Compound fName args) =
