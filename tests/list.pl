eb74781daa3aecbeb2493a00e03a7a0f3eff2e9b
osa1 <omeragacan@gmail.com>
2013-07-15 12:59:35 +0300

    moar std funs

--- tests/list.pl
+++ tests/list.pl
@@ -1,2 +1,5 @@
 member(A, #cons(A, _)).
 member(A, #cons(_, Rest)) :- member(A, Rest).
+
+concat(nil, A, A).
+concat(#cons(H, T), L, #cons(H, R1)) :- concat(T, L, R1).
--- tests/peano.pl
+++ tests/peano.pl
@@ -22,3 +22,11 @@ lt(s(A), s(B)) :- lt(A, B).
 % greater than
 gt(s(_), o).
 gt(s(A), s(B)) :- gt(A, B).
+
+% greater than/equal
+gte(A, B) :- gt(A, B).
+gte(A, B) :- eqNat(A, B).
+
+% less than/equal
+lte(A, B) :- lt(A, B).
+lte(A, B) :- eqNat(A, B).
